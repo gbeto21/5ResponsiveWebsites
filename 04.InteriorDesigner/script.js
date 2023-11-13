d333e0bcebf7089cd74b1d85556515d3a7dc7be1
Alberto <albertoglezb@hotmail.com>
2023-11-13 10:33:28 -0600

    The services section was added.

--- 04.InteriorDesigner/script.js
+++ 04.InteriorDesigner/script.js
@@ -12,9 +12,16 @@ xBtn.addEventListener("click", () => {
 
 window.addEventListener("scroll", () => {
   const about = document.querySelector(".about");
+  const services = document.querySelector(".services");
   if (window.pageYOffset >= 200) {
     about.classList.add("change");
   } else {
     about.classList.remove("change");
+  }
+
+  if (window.pageYOffset >= about.offsetTop + 200) {
+    services.classList.add("change");
+  } else {
+    services.classList.remove("change");
   }
 });

0f71092a1f3cc958b8386dae81779d6e7ab33d24
Alberto <albertoglezb@hotmail.com>
2023-11-13 10:14:06 -0600

    The about section was added.

--- 04.InteriorDesigner/script.js
+++ 04.InteriorDesigner/script.js
@@ -1,11 +1,20 @@
-const menuIcon = document.querySelector(".menu-icon")
-const xBtn = document.querySelector(".x-btn")
-const navigation = document.querySelector(".navigation")
+const menuIcon = document.querySelector(".menu-icon");
+const xBtn = document.querySelector(".x-btn");
+const navigation = document.querySelector(".navigation");
 
-menuIcon.addEventListener("click", ()=>{
-  navigation.classList.add("navigate")
-})
+menuIcon.addEventListener("click", () => {
+  navigation.classList.add("navigate");
+});
 
-xBtn.addEventListener("click", ()=>{
-  navigation.classList.remove("navigate")
-})
+xBtn.addEventListener("click", () => {
+  navigation.classList.remove("navigate");
+});
+
+window.addEventListener("scroll", () => {
+  const about = document.querySelector(".about");
+  if (window.pageYOffset >= 200) {
+    about.classList.add("change");
+  } else {
+    about.classList.remove("change");
+  }
+});
